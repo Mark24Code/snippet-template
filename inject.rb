7f8c633d1b10d4dd9d39d2835fd0730884741556
Mark24 <mark.zhangyoung@qq.com>
2022-07-16 11:55:56 +0800

    feat: refactor inject

--- inject.rb
+++ inject.rb
@@ -1,26 +1,27 @@
 
 require './render'
 
-module InjectCommon
-  def write_result(output,content)
-    File.open("#{output}", 'w') { |f|
-      f << content
-    }
-  end
-end
+module SnippetTemplate
+  class Inject
+    include InjectCommon
 
-class Inject
-  include InjectCommon
+    def << self
+      def write_result(output,content)
+        File.open("#{output}", 'w') { |f|
+          f << content
+        }
+      end
+    end
 
-  def initialize(jobs)
-    @jobs = jobs || []
-  end
+    def initialize(jobs)
+      @jobs = jobs || []
+    end
 
-  def start
-    @jobs.each do |job|
-      content = render({name: job[:snippet], props: job[:props]})
-      write_result(job[:output], content)
+    def start
+      @jobs.each do |job|
+        content = render({name: job[:snippet], props: job[:props]})
+        self.class.write_result(job[:output], content)
+      end
     end
   end
 end
-
