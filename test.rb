c079fe61b3f93fa36ed2284a7c9d3e82ea1181e6
Mark24 <mark.zhangyoung@qq.com>
2022-07-16 08:27:20 +0800

    feat: pure template nest works

--- test.rb
+++ test.rb
@@ -1,3 +1,19 @@
 require './render'
 
+# 外部render 和 内部render 保持一致
+# 第一次运行的是外部render，一旦开始是内部class绑定的render，达到概念的一致性
+# name 是约定的 sinippet 中文件名 symbol
+# 文件名小写、内部template存在约定
+
+# 约定module范式、接口view
+#
+# module SnippetTemplate
+#   class Title
+#     def view
+#     return %{
+#       >>>> title <<<<
+#     }
+#     end
+#   end
+# end
 puts render(:demo)
